abd08c1c7850b39f20ae9c98cfd3b010e8f14e1b
shima <taiyaki1275@gmail.com>
2026-09-13 18:04:32 +0900

    feat: load theta values from file

--- predict.py
+++ predict.py
@@ -2,9 +2,17 @@ def estimate_price(mileage, theta0, theta1):
     return theta0 + theta1 * mileage
 
 
+def load_theta(path):
+    try:
+        with open(path) as f:
+            theta0, theta1 = f.read().split(",")
+            return float(theta0), float(theta1)
+    except FileNotFoundError:
+        return 0.0, 0.0
+
+
 def main():
-    theta0 = 0
-    theta1 = 0
+    theta0, theta1 = load_theta("theta.txt")
 
     mileage = float(input("走行距離を入力してください: "))
     price = estimate_price(mileage, theta0, theta1)
